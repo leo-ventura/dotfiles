0dfa2f2a11c098ddcd42b018db16e6caa4cf280e
Leonardo Ventura <leonardo.venturacl@gmail.com>
2020-09-24 14:17:34 -0300

    Show only spotify player on polybar

--- .config/polybar/scripts/play-pause.sh
+++ .config/polybar/scripts/play-pause.sh
@@ -1,3 +1,9 @@
 #/usr/bin/bash
 
-echo -n ""
+status=$(playerctl -p spotify status 2> /dev/null)
+
+if [[ $status = "Playing" ]]; then
+	echo -n "%{F#65737E}"
+elif [[ $status = "Paused" ]]; then
+	echo -n "%{F#000}"
+fi
